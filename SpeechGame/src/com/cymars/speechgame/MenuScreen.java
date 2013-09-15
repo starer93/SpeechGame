3aa50e9dcf6f70bd8b053898f512f713517d4318
Richard Yu <starer93@hotmail.com>
2013-09-15 15:47:24 +1000

    speechGame

---
+++ SpeechGame/src/com/cymars/speechgame/MenuScreen.java
@@ -0,0 +1,18 @@
+package com.cymars.speechgame;
+
+import junit.framework.TestCase;
+
+public class MenuScreen extends TestCase {
+
+	public MenuScreen(String name) {
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+}
